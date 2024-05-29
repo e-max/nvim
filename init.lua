dd51c765b47d794ee94845988f6e8212db32c02e
Maxim <maxim@btcx.com>
2024-05-29 21:50:56 +0200

    some colorscheme stuff

--- init.lua
+++ init.lua
@@ -906,12 +906,62 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'jellybeans'
+      -- vim.cmd.colorscheme 'jellybeans'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      --   vim.cmd.hi 'Comment gui=none'
     end,
   },
+
+  {
+    'metalelf0/jellybeans-nvim',
+    lazy = false,
+    priority = 1000,
+
+    dependencies = {
+      {
+        'rktjmp/lush.nvim',
+      },
+    },
+    config = function() end,
+    init = function()
+      vim.cmd.colorscheme 'jellybeans-nvim'
+      -- You can configure highlights by doing something like:
+      -- vim.cmd.hi 'Type gui=none'
+      local lush = require 'lush'
+      local jb = require 'lush_theme.jellybeans-nvim'
+      local hsl = lush.hsl
+
+      -- Now we will extend the colorscheme
+      local spec = lush.extends({ jb }).with(function()
+        return {
+          -- make the telescope popup look like the pmenu
+          FlashLabel { cterm = 'bold', gui = 'bold', fg = hsl '#a6e3a2', bg = hsl '#1e1e2f' },
+          Substitute { bg = hsl(208, 100, 80), fg = hsl(1, 100, 80) }, -- |:substitute| replacement text highlighting
+          -- Search { bg = hsl(208, 100, 80), fg = hsl(1, 100, 80) }, -- |:substitute| replacement text highlighting
+          Type { fg = hsl '#ffb964', gui = 'none' }, -- (preferred) int, long, char, etc.
+        }
+      end)
+
+      -- then pass the extended spec to lush for application
+      return lush(spec)
+    end,
+  },
+
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      --vim.cmd.colorscheme 'gruvbox-material'
+    end,
+  },
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  { 'ellisonleao/gruvbox.nvim', name = 'gruvbox', priority = 1000, config = true },
+  { 'savq/melange-nvim', priority = 1000 },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
