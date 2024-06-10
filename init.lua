b7784049eb1dffc1388b0a959b1300cfd289707f
Maxim <maxim@btcx.com>
2024-06-10 21:34:10 +0200

    switch from friendly-snippets to vim-snippets

--- init.lua
+++ init.lua
@@ -803,10 +803,16 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
+          -- {
+          --   'rafamadriz/friendly-snippets',
+          --   config = function()
+          --     require('luasnip.loaders.from_vscode').lazy_load()
+          --   end,
+          -- },
           {
-            'rafamadriz/friendly-snippets',
+            'honza/vim-snippets',
             config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
+              require('luasnip.loaders.from_snipmate').lazy_load()
             end,
           },
         },
