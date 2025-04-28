fae24e7d49c39cc9fd4998b23b27df183bb840d0
Maxim <maxim@btcx.com>
2025-04-28 17:11:39 +0200

    config rustaceanvim to make use RA separate folder

--- lua/custom/plugins/rustaceanvim.lua
+++ lua/custom/plugins/rustaceanvim.lua
@@ -2,4 +2,33 @@ return {
   'mrcjkb/rustaceanvim',
   version = '^6', -- Recommended
   lazy = false, -- This plugin is already lazy
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        on_attach = function(client, bufnr)
+          -- you can also put keymaps in here
+        end,
+        default_settings = {
+          ['rust-analyzer'] = {
+
+            assist = {
+              importMergeBehavior = 'last',
+              importPrefix = 'by_self',
+            },
+            cargo = {
+              loadOutDirsFromCheck = true,
+              allFeatures = true,
+              targetDir = true,
+            },
+            --          trace = {
+            --           	server = "verbose",
+            --			},
+            -- checkOnSave = {
+            --   extraArgs = { '--target-dir', rust_analyzer_folder },
+            -- },
+          },
+        },
+      },
+    }
+  end,
 }
