00cb41b10f275d246871dc8d3d9c1a722a9a7e89
Max Lavrenov <max.lavrenov@gmail.com>
2026-05-18 15:04:08 +0200

    replacer uses \r

--- lua/custom/plugins/replacer.lua
+++ lua/custom/plugins/replacer.lua
@@ -2,4 +2,4 @@ vim.pack.add { 'https://github.com/gabrielpoca/replacer.nvim' }
 
 require('replacer').setup { rename_files = false }
 
-vim.keymap.set('n', '<leader>h', function() require('replacer').run() end, { desc = 'run replacer.nvim' })
+vim.keymap.set('n', '<localleader>r', function() require('replacer').run() end, { desc = 'run replacer.nvim' })
